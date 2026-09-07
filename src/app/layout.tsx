ff2f6d8c18ac2029a04e7c28b1608095a2533750
이원빈 <ldcumg@naver.com>
2026-09-08 00:56:49 +0900

    fix: 폰트 파일 경로 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 
 const pretendard = localFont({
-  src: "./assets/fonts/PretendardVariable.woff2",
+  src: "./../assets/fonts/PretendardVariable.woff2",
 });
 
 export const metadata: Metadata = {
