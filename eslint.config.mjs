b016ead2e1ba9b867cac5ec311440a1a1fb904ca
이원빈 <ldcumg@naver.com>
2026-09-08 00:53:41 +0900

    chore: storybook 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,14 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier/flat";
+import storybook from "eslint-plugin-storybook";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -18,6 +22,7 @@ const eslintConfig = defineConfig([
       "@next/next/no-img-element": "off",
     },
   },
+  ...storybook.configs["flat/recommended"],
 ]);
 
 export default eslintConfig;
